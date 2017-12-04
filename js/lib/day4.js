524c6e25abae6043f82af838fbaab81d72e28388
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2017-12-04 11:11:59 +0100

    Day 4.

---
+++ js/lib/day4.js
@@ -0,0 +1,20 @@
+const toArr = (line) => line.split(/\s/)
+
+const isValid = (arr) => {
+  // Handle the last empty line
+  if (arr.length === 1 && arr[0] === '') {
+    return false
+  }
+  return arr.length === Object.keys(arr.reduce((acc, e) => {
+    acc[e] = true
+    return acc
+  }, {})).length
+}
+
+const passphrase = (input) => input.split('\n').map(toArr).map(isValid).filter((a) => a).length
+
+module.exports = {
+  toArr,
+  isValid,
+  passphrase
+}
---
+++ js/test/day4.test.js
@@ -0,0 +1,28 @@
+const { toArr, isValid, passphrase } = require('../lib/day4')
+const FS = require('fs')
+
+test('day 4 - isValid sample', () => {
+  expect(isValid(toArr('aa bb cc dd ee'))).toEqual(true)
+  expect(isValid(toArr('aa bb cc dd aa'))).toEqual(false)
+  expect(isValid(toArr('aa bb cc dd aaa'))).toEqual(true)
+})
+
+test('isValid', () => {
+  expect(isValid(['kvvfl', 'kvvfl', 'olud', 'wjqsqa', 'olud', 'frc'])).toBe(false)
+})
+
+test('day 4 - sample', () => {
+  const input = `aa bb cc dd ee
+aa bb cc dd aa
+aa bb cc dd aaa`
+  expect(passphrase(input)).toEqual(2)
+})
+
+test('day 4 - main', () => {
+  const input = readSampleData()
+  expect(passphrase(input)).toEqual(337)
+})
+
+const readSampleData = (filename = 'day4_input.txt') => {
+  return FS.readFileSync(`${__dirname}/${filename}`).toString()
+}
