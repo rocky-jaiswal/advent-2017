b8308075a671d2f4f9699935fa1d4e1e20f879ee
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2017-12-09 12:26:12 +0100

    Day 9 part 1.

---
+++ js/lib/day9.js
@@ -0,0 +1,79 @@
+const { readSampleData } = require('./utils')
+
+const parseSum = (filename = 'day9_sample.txt') => {
+  const input = readSampleData(filename).split('').filter((c) => c.trim() !== '')
+
+  let groupStarted = false
+  let garbageStarted = false
+  let ignoreStarted = false
+  let group = []
+  let nested = 0
+  const groups = []
+
+  input.forEach((i) => {
+    if (!ignoreStarted) {
+      if (i === '!') {
+        ignoreStarted = true
+        return
+      }
+      if (i === '<') {
+        garbageStarted = true
+        return
+      }
+      if (i === '>') {
+        garbageStarted = false
+        return
+      }
+      if (!garbageStarted) {
+        if (!groupStarted && i === '{') {
+          nested = nested + 1
+          groupStarted = true
+          group.push(i)
+          return
+        }
+        if (groupStarted && i === '{') {
+          nested = nested + 1
+          group.push(i)
+          return
+        }
+        if (groupStarted && i === '}') {
+          group.push(i)
+          nested = nested - 1
+          if (nested === 0) {
+            groups.push(group)
+            groupStarted = false
+            group = Array.from([])
+          }
+        }
+      }
+    } else {
+      ignoreStarted = false
+    }
+  })
+
+  // console.log(groups.map((g) => g.join('')))
+
+  return groups
+    .map((g) => g.join(''))
+    .map((grp) => {
+      const chars = grp.split('')
+      const nested = []
+      let n = 0
+      chars.forEach((c) => {
+        if (c === '{') {
+          n = n + 1
+        }
+        if (c === '}') {
+          nested.push(n)
+          n = n - 1
+        }
+      })
+      return nested
+    })
+    .map((g) => g.reduce((acc, e) => acc + e))
+    .reduce((acc, e) => acc + e)
+}
+
+module.exports = {
+  parseSum
+}
---
+++ js/test/day9.test.js
@@ -0,0 +1,5 @@
+const { parseSum } = require('../lib/day9')
+
+test('day 9 - sample', () => {
+  expect(parseSum()).toBe(51)
+})
